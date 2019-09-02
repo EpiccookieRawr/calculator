92dfdeb83bf484280be998bd202aca560269d54e
Chien Wei Chou <chien.chou@numinix.com>
2019-09-02 12:27:41 -0700

    Fix bug in functionTwoParam, Add state, fix display equation

--- app.js
+++ app.js
@@ -2,22 +2,10 @@ const dataCtrl = (function(){
     const dictionary = {
         'operator' : ['*', '-', '+', '/'],
         'number' : ['1','2','3','4','5','6','7','8','9','0', '.'],
-        'functions' : ['sin', 'cos', 'tan', 'ln', 'log', 'sqrt'],
+        'functions' : ['sin', 'cos', 'tan', 'ln', 'log', 'sqrt', 'pow'],
         'functionsTwoParams' : ['^'],
         'constants' : ['PI', 'E', 'Ans'],
         'brackets' : ['(', ')']
-    }
-
-    const insertFunctionMethod = {
-        '^' : function(equation, indexes) {
-            console.log(indexes);
-            if(indexes.length > 0) {
-                equation.splice(indexes[0], 1, ',');
-                equation.splice(indexes[1], 0, 'pow', '(');
-                equation.push(')');
-            }
-            return equation;
-        }
     }
 
     const translation = {
@@ -34,10 +22,10 @@ const dataCtrl = (function(){
 
     const dictionaryRules = {
         'operator' : {
-            'correctInputs' : ['number', 'functions', 'brackets', 'functionsTwoParams', 'operator']
+            'correctInputs' : ['number', 'functions', 'brackets', 'functionsTwoParams', 'operator', 'constants']
         },
         'number' :{
-            'correctInputs' : ['number', 'functions', 'decimals', 'operator', 'brackets', 'functionsTwoParams']
+            'correctInputs' : ['number', 'functions', 'decimals', 'operator', 'brackets', 'functionsTwoParams', 'constants']
         },
         'functions' : {
             'correctInputs' : ['number', 'decimals', 'functions', 'constants', 'brackets']
@@ -53,6 +41,18 @@ const dataCtrl = (function(){
         }
     }
 
+    const insertFunctionMethod = {
+        '^' : function(equation, indexes) {
+            console.log(indexes);
+            if(indexes.length > 0) {
+                equation.splice(indexes[0], 1, ',');
+                equation.splice(indexes[1], 0, 'pow', '(');
+                equation.splice(indexes[2], 0, ')');
+            }
+            return equation;
+        }
+    }
+
     const data = {
         'equation' : [],
         'currentType' : null,
@@ -64,10 +64,6 @@ const dataCtrl = (function(){
 
     const getEquation = function() {
         return data.equation;
-    }
-
-    const logData = function() {
-        return data;
     }
 
     const addInput = function(input) {
@@ -89,6 +85,7 @@ const dataCtrl = (function(){
                             }
                             break;
                         case 'functions' :
+                        case 'functionsTwoParams' :
                             data.equation.push(input, '(');
                             break;
                         default:
@@ -100,7 +97,6 @@ const dataCtrl = (function(){
                 }
             }
         }
-        console.log(data.equation);
         return true;
     }
 
@@ -112,16 +108,16 @@ const dataCtrl = (function(){
             if(dictionary.functionsTwoParams.indexOf(equationElement) !== -1) {
                 let bracketCount = 0;
                 let stopIndex = 0;
-                let numberCount = 0;
+                let numberCount;
                 let beginningIndex = 0;
                 const insertFunction = {
                     type : equationElement,
                     indexes : [equationIndex]
                 };
-
-                if(dictionary.number.indexOf(data.equation[equationIndex - 1]) !== -1) {
+                if([...dictionary.constants,...dictionary.number].indexOf(data.equation[equationIndex - 1]) !== -1) {
                     stopIndex = 1;
-                    while(dictionary.number.indexOf(data.equation[equationIndex - stopIndex]) !== -1) {
+                    numberCount = 0;
+                    while([...dictionary.constants,...dictionary.number].indexOf(data.equation[equationIndex - stopIndex]) !== -1) {
                         numberCount++;
                         stopIndex++;
                     }
@@ -147,6 +143,27 @@ const dataCtrl = (function(){
                         insertFunction.indexes.push(beginningIndex);
                     }
                 }
+
+                bracketCount = 1;
+                stopIndex = 1;
+                if(dictionary.brackets.indexOf(data.equation[equationIndex + stopIndex]) !== -1){
+                    while(bracketCount !== 0) {
+                        if((data.equation[equationIndex + stopIndex]) !== undefined) {
+                            stopIndex++;
+                            if (data.equation[equationIndex + stopIndex] === ')') {
+                                bracketCount--;
+                            } else if (data.equation[equationIndex + stopIndex] === '('){
+                                bracketCount++;
+                            }
+                        } else {
+                            break;
+                        }
+                    }
+                    beginningIndex = equationIndex + stopIndex;
+                    const offSet = (insert.length + 1) * 3;
+                    insertFunction.indexes.push(beginningIndex + offSet);
+                }
+
                 insert.unshift(insertFunction);
             }
 
@@ -154,20 +171,18 @@ const dataCtrl = (function(){
         });
 
         if(insert.length > 0) {
-            insert.forEach(insertFunc => {
+            insert.forEach((insertFunc) => {
                 if(insertFunctionMethod[insertFunc.type] !== null) {
                    finalEquation = insertFunctionMethod[insertFunc.type](finalEquation,insertFunc.indexes);
                 }
            });
         }
 
-        console.log(finalEquation);
-
         const multiplyIndexes = [];
         finalEquation.forEach((finalElement, finalIndex) => {
             //handle implicit multiplication
             if(finalElement == ')') {
-                if([...dictionary.functions, ...dictionary.brackets,...dictionary.constants,...dictionary.number].indexOf(finalEquation[finalIndex+1]) !== -1){
+                if([...dictionary.functions, '(',...dictionary.constants,...dictionary.number].indexOf(finalEquation[finalIndex+1]) !== -1){
                     multiplyIndexes.unshift(finalIndex+1);
                 }
             }
@@ -189,10 +204,12 @@ const dataCtrl = (function(){
             });
         }
 
-        console.log(finalEquation);
-
+        let result = finalEquation.join('');
+        console.log(result);
+        result = eval(result);
+        console.log(result);
+        data.equation = [result];
         return;
-        data.equation = [{element : result, type : 'number'}];
     }
 
     const clearData = function() {
@@ -204,7 +221,6 @@ const dataCtrl = (function(){
 
     return {
         getEquation,
-        logData,
         addInput,
         evalEquation,
         clearData
@@ -222,10 +238,39 @@ const UICtrl = (function(){
         'buttonsContainer' : '.buttons-container'
     }
 
+    displayTranslation = {
+        '/' : {
+           html : '<i class="fas fa-divide"></i>',
+           class : 'dark-button-color'
+        },
+        '*' : {
+           html : '<i class="fas fa-times"></i>',
+           class : 'dark-button-color'
+        },
+        '-' : {
+           html : '<i class="fas fa-minus"></i>',
+           class : 'dark-button-color'
+        },
+        '+' : {
+           html : '<i class="fas fa-plus"></i>',
+           class : 'dark-button-color'
+        },
+        '=' : {
+           html : '<i class="fas fa-equals"></i>',
+           class : 'dark-button-color'
+        },
+    }
+
+    const currentState = {
+        state : 'basic'
+    }
+
     const functionLayoutSequence = ['Ans', 'sin', 'cos', 'tan', 'ln', 'log', 'sqrt', 'PI', 'E', '^', '(', ')'];
 
+    const basicLayoutSequence = ['7', '8', '9', '/', '4', '5','6','*','1','2','3','-','.','0','=','+'];
+
     const functionLayout = function() {
-        //removeButtons();
+        removeButtons();
         const buttonsContainer = document.querySelector(UISelectors.buttonsContainer);
         functionLayoutSequence.forEach(functionButton => {
             const li = document.createElement('li');
@@ -236,6 +281,27 @@ const UICtrl = (function(){
             li.style.width = '32%';
             buttonsContainer.appendChild(li);
         });
+        currentState.state = 'function';
+    }
+
+    const basicLayout = function() {
+        removeButtons();
+        const buttonsContainer = document.querySelector(UISelectors.buttonsContainer);
+        basicLayoutSequence.forEach(functionButton => {
+            const li = document.createElement('li');
+            const button = document.createElement('button');
+            button.setAttribute('value', functionButton);
+            if(displayTranslation[functionButton] !== undefined) {
+                button.innerHTML = displayTranslation[functionButton].html;
+                button.className = displayTranslation[functionButton].class;
+            } else {
+                button.innerHTML = functionButton;
+            }
+            li.appendChild(button);
+            li.style.width = '24%';
+            buttonsContainer.appendChild(li);
+        });
+        currentState.state = 'basic';
     }
 
     const removeButtons = function(){
@@ -246,14 +312,19 @@ const UICtrl = (function(){
     }
 
     const displayEquation = function(equation) {
-        const equationElements = equation.map(equationElement => {return equationElement.element});
-        document.querySelector(UISelectors.displayResult).textContent = equationElements.join('');
+        document.querySelector(UISelectors.displayResult).textContent = equation.join('');
+    }
+
+    const getCurrentState = function() {
+        return currentState;
     }
 
     return {
         UISelectors,
         displayEquation,
-        functionLayout
+        functionLayout,
+        basicLayout,
+        getCurrentState
     }
 })();
 
@@ -261,7 +332,6 @@ const appCtrl = (function(dataCtrl, UICtrl){
     const htmlSelectors = UICtrl.UISelectors;
 
     const loadedEventListeners = function() {
-        UICtrl.functionLayout();
         let buttons = document.querySelectorAll(htmlSelectors.calculatorButtons);
         const deleteButton = document.querySelector(htmlSelectors.deleteButton);
         const moreButton = document.querySelector(htmlSelectors.moreButton);
@@ -270,7 +340,7 @@ const appCtrl = (function(dataCtrl, UICtrl){
             button.addEventListener('click', updateEquation); 
         });
         deleteButton.addEventListener('click', clearData);
-        moreButton.addEventListener('click', changeFunState);
+        moreButton.addEventListener('click', changeState);
     }
 
     const clearData = function(){
@@ -278,8 +348,13 @@ const appCtrl = (function(dataCtrl, UICtrl){
         UICtrl.displayEquation(dataCtrl.getEquation());
     }
 
-    const changeFunState = function(){
-        UICtrl.functionLayout();
+    const changeState = function(){
+        const currentState = UICtrl.getCurrentState().state;
+        if(currentState === 'basic'){
+            UICtrl.functionLayout();
+        } else {
+            UICtrl.basicLayout();
+        }
     }
 
     const updateEquation = function(e) {
